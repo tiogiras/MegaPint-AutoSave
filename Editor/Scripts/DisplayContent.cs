a64ff2a18d58f7fc709509192b1cefa45b7df1dd
Tiogiras <tiogiras@gmail.com>
2023-08-28 16:27:55 +0200

    Improved Visuals

---
+++ Editor/Scripts/DisplayContent.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace Editor.Scripts
+{
+    public static partial class DisplayContent
+    {
+        private static partial void AutoSave()
+        {
+            // TODO this
+            Debug.Log("TODO: AutoSave Display Logic");
+        }
+    }
+}
+#endif
