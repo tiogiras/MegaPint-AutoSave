96f1ec4d07387b2c2f7634929c5c0fb41dacf981
Tiogiras <tiogiras@gmail.com>
2024-06-06 14:09:55 +0200

    Added package specific tests

---
+++ Editor/Scripts/Tests/MenuItemTests.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using MegaPint.Editor.Scripts.Tests.Utility;
+using MegaPint.Editor.Scripts.Windows;
+using NUnit.Framework;
+
+namespace MegaPint.Editor.Scripts.Tests
+{
+
+/// <summary> Unit tests regarding the menuItems of the package </summary>
+internal class MenuItemTests
+{
+    #region Tests
+
+    [Test] [Order(1)]
+    public void AutoSave()
+    {
+        TestsUtility.ValidateMenuItemLink(Constants.AutoSave.Links.AutoSaveWindow, typeof(AutoSave));
+    }
+
+    #endregion
+}
+
+}
+#endif
